1c643603c7398b766db424bf466104dc00c3ca1e
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2022-02-02 19:49:42 -0300

    adicionando gif

--- index.js
+++ index.js
@@ -8,35 +8,55 @@ document.querySelector("#btn").onclick = function() {
     const h1 = $("h1");
     const header = $(".header");
     const info = $(".info");
+    const gif = $("#gif")
 
+ 
     var input = document.querySelector("input").value;
+
 
     $.ajax({
     url:`https://api.nasa.gov/planetary/apod?api_key=OcZL1me94KWDsvoTYSDGj8Wkigga6YIjftaGpTbP&date=${input}`,
     success: function(response){
-        
+        document.getElementById("gif").style.display = "none";
         titulo.text(response.title);
         descricao.text(response.explanation);
         if(response.copyright !== undefined){
             autor.text(response.copyright);
+
         }else{
             autor.text(`Autor desconhecido`);
         }
 
-        if(response.media_type == "image"){
+        if(response.media_type == "image"){           
             imagem.attr("src",response.url);
             document.getElementById("iframe").style.display = "none";
             document.getElementById("imagem").style.display = "block";
+            titulo.css("color", "darkorange");
+            imagem.css("borderColor", "darkorange")
+            footer.css("color", "darkorange");
+            h1.css("color", "darkorange");
+            info.css("color", "darkorange");
+            info.css("borderColor", "darkorange");
+            header.css("borderColor", "darkorange");
         }else{
             iframe.attr("src", response.url);
             document.getElementById("imagem").style.display = "none";
             document.getElementById("iframe").style.display = "block";
+            titulo.css("color", "darkorange");
+            imagem.css("borderColor", "darkorange")
+            footer.css("color", "darkorange");
+            h1.css("color", "darkorange");
+            info.css("color", "darkorange");
+            info.css("borderColor", "darkorange");
+            header.css("borderColor", "darkorange");
         }
         return response;
     },
 
     error: function(){
-        titulo.text(`ERROR:Insira uma data de 16/06/1995 até a data de hoje ou não carregou por um erro na API, RECARREGUE A PÁGINA PARA TENTAR NOVAMENTE`);
+        document.getElementById("gif").style.display = "none";
+        titulo.text(`ERROR:Insira uma data de 16/06/1995 até a data de hoje ou não carregou 
+        por um erro na API, RECARREGUE A PÁGINA PARA TENTAR NOVAMENTE`);
         titulo.css("border", "dashed","3px");
         titulo.css("color", "red");
         imagem.css("borderColor", "red")
@@ -53,5 +73,6 @@ document.querySelector("#btn").onclick = function() {
         descricao.css("display", "none");
         autor.css("display", "none");
     },
+
 })
 } 
